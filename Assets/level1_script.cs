bcf6e24109c96d2da4bbd0d8affa51b3b23f53d2
faical23 <65980103+faical23@users.noreply.github.com>
2020-10-05 23:23:45 +0200

    i add script

---
+++ Assets/level1_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class level1_script : MonoBehaviour
+{
+// Start is called before the first frame update
+void Start()
+{
+
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+public void back_to_menu()
+{
+SceneManager.LoadScene("menu"); //// teeeeeeeeeeeeeest
+}
+}
