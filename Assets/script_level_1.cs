e63e1c98447fff14c3026cfac2054a2139263145
faical23 <65980103+faical23@users.noreply.github.com>
2020-10-12 16:23:06 +0200

    fgfgfgffgfg

--- Assets/script_level_1.cs
+++ Assets/script_level_1.cs
@@ -5,25 +5,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-
-public class level1_script : menu_script
+public class script_level_1 : script_menu
 {
-    //public menu_script menu_Script;
-    void start(){
 
+    private void Update() {
+        totalcoin_level1.text = total.ToString();
     }
 public void back_to_menu()
 {
 SceneManager.LoadScene("menu"); //// teeeeeeeeeeeeeest
 }
 
-
 ///// counter coins 
     public TextMeshProUGUI totalcoin_level1;
     
-    public void score_level1(){
-            Debug.Log("scoor :" + scoor);
-           // scoor++;
-
+    public void score_level_1(){
+        totalcoin_level1.text = total.ToString();
+        Debug.Log("scoor :" + total);
+        total++;
     }
+    
 }
---
+++ Assets/script_menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class script_menu : MonoBehaviour
+{
+/////////////////// buttons
+
+public void playgame()
+{
+SceneManager.LoadScene("level1"); //// for go to the level1
+}
+public void exitgame(){
+}
+
+
+    public TextMeshProUGUI totalcoin;
+    public static int scoore = 0;
+    public void score_game(){
+        Debug.Log("scoor :" + scoore);
+        totalcoin.text = scoore.ToString(); /// score menu
+    }
+    public int total = scoore; /// all score in menu
+
+}
