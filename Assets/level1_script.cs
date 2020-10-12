c2ea43b3d86ebe8c5ab399349c5b851624aa0627
faical23 <65980103+faical23@users.noreply.github.com>
2020-10-12 14:32:04 +0200

    jkk

--- Assets/level1_script.cs
+++ Assets/level1_script.cs
@@ -6,13 +6,11 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class level1_script : MonoBehaviour
+public class level1_script : menu_script
 {
     //public menu_script menu_Script;
     void start(){
-        //GameObject thePlayer = GameObject.Find("ThePlayer");
-        //menu_script menu_Script = thePlayer.GetComponent<menu_script>();
-        //menu_Script.scoor = 10;
+
     }
 public void back_to_menu()
 {
@@ -21,21 +19,11 @@ SceneManager.LoadScene("menu"); //// teeeeeeeeeeeeeest
 
 
 ///// counter coins 
-    //public int scoor = 0;
     public TextMeshProUGUI totalcoin_level1;
     
-     //public int scoor_2 = 0;
     public void score_level1(){
-           // GameObject thePlayer = GameObject.Find("ThePlayer");
-            //menu_script menu_Script = thePlayer.GetComponent<menu_script>();
-            //menu_Script.scoor = 10;
-            //scoor_2 =+ menu_Script.scoor;
-           // int test = menu_Script.scoor;
-            //int scoor_level1 = menu_Script.scoor;
-            //menu_Script.scoor;
-            //totalcoin_level1.text = scoor_level1.ToString();
-            Debug.Log("scoor : " + scoor_2);
-            //scoor_level1++;
-           // Debug.Log("is woorkiing");
+            Debug.Log("scoor :" + scoor);
+           // scoor++;
+
     }
 }
