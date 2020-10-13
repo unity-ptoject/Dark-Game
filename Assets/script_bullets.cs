19ba23aaffb36e0e60f229bfd9cab28c7aaf85ce
faical23 <65980103+faical23@users.noreply.github.com>
2020-10-13 14:15:14 +0200

    kdkksdds

---
+++ Assets/script_bullets.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_bullets : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int enemy_dead = 3;
+    void Start()
+    {
+        rb.velocity = transform.right * speed ;
+    }
+
+    ///// killer enemey
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+
+        if (other.gameObject.tag == "enemy") 
+        {
+            if(enemy_dead > 0)
+            {
+                enemy_dead--;
+                Destroy(other.gameObject);
+                Debug.Log(" u killer enemy");
+            }
+
+        }
+
+    }
+}
