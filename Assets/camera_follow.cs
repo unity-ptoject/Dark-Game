cde19b1a912eb31b82cf8e17498a31f0a1d98006
faical23 <65980103+faical23@users.noreply.github.com>
2021-01-06 11:05:14 +0100

    add camera folow

--- Assets/camera_follow.cs
+++ Assets/camera_follow.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class camera_follow : MonoBehaviour
 {
-    public Transform player;
-      public Vector3 offset;
-      
-      void Update () 
-      {
-          transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z); 
-          // Camera follows the player with specified offset position
-      }
+   
+	public float interpVelocity;
+	public float minDistance;
+	public float followDistance;
+	public GameObject target;
+	public Vector3 offset;
+	Vector3 targetPos;
+	// Use this for initialization
+	void Start () {
+		targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (target)
+		{
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+
+			interpVelocity = targetDirection.magnitude * 5f;
+
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
+
+			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
+
+		}
+	}
 }
